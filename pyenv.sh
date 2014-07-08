3521e4d61fbb6e0932aca56fd23a04155001db96
puhitaku <puhitaku@gmail.com>
2014-07-08 13:02:10 -0700

    changed arrange

--- pyenv.sh
+++ pyenv.sh
@@ -4,14 +4,14 @@ echo "Installing pyenv..."
 sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm
 curl -L https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash
 
+echo "Installing pyenv-pip-rehash and pyenv-virtualenv..."
+git clone https://github.com/yyuu/pyenv-pip-rehash.git ~/.pyenv/plugins/pyenv-pip-rehash
+git clone https://github.com/yyuu/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-virtualenv
+
 echo "Adding the path to pyenv..."
 echo "export PATH=\"~/.pyenv/bin:\$PATH\"" >> ~/.bashrc
 eval "$(~/.pyenv/bin/pyenv init - >> ~/.bashrc)"
 eval "$(~/.pyenv/bin/pyenv virtualenv-init - >> ~/.bashrc)"
-
-echo "Installing pyenv-pip-rehash and pyenv-virtualenv..."
-git clone https://github.com/yyuu/pyenv-pip-rehash.git ~/.pyenv/plugins/pyenv-pip-rehash
-git clone https://github.com/yyuu/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-virtualenv
 
 echo "Done! Please do \"source ~/.bashrc\" and it will take a effect."
 
