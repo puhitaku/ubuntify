c5eec68eae2b856677fc5edf6106106aae09dcf2
puhitaku <puhitaku@gmail.com>
2014-07-08 10:08:07 -0700

    some corrections

--- base.sh
+++ base.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
+SSHDIR="~/.ssh"
+
 echo "PPA repos are going to be installed. Proceed with installation pressing Enter."
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
 sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
 sudo apt-get update
 
-echo "Going to install GNOME Panel, vim, Guake and Chrome."
-sudo apt-get install gnome-panel vim guake google-chrome-stable
+echo "Installing GNOME Panel, vim, Guake, Chrome and additional tools..."
+sudo apt-get -y install git gnome-panel vim guake google-chrome-stable xclip
+
+echo "Logout this session and re-login with GNOME Panel."
+
