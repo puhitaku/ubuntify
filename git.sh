64a3c869863e27075887a0a97b3b797e2ba10260
puhitaku <puhitaku@gmail.com>
2014-07-08 10:40:04 -0700

    almost complete

--- git.sh
+++ git.sh
@@ -1,26 +1,27 @@
 #!/bin/sh
 
-USER="who the fuck are you"
-EMAIL="what_the@fuck.com"
-
-if test $USER = "who the fuck are you" -o $EMAIL = "what_the@fuck.com" ; then
-  echo "Write your username and e-mail address in this script. Aborting."
-  exit
-fi
+echo "Your name on github?"
+read USER
+echo "Your E-mail address?"
+read EMAIL
+SSHDIR=~/.ssh
 
 echo "Setting up git..."
+echo "Username =" $USER
+echo "E-mail =" $EMAIL
+
 git config --global user.name "$USER"
-git config --global user.email EMAIL
+git config --global user.email $EMAIL
 
 echo "Generating SSH key. The passphrase is not needed so just press Enter some times."
 ssh-keygen
 chmod 600 $SSHDIR/id_rsa
 
 touch $SSHDIR/config
-echo "Host github.com" > $SSHDIR/config
-echo "    HostName github.com" > $SSHDIR/config 
-echo "    IdentifyFile "$SSHDIR/id_rsa > $SSHDIR/config
-echo "    User git" > $SSHDIR/config
+echo "Host github.com" >> $SSHDIR/config
+echo "    HostName github.com" >> $SSHDIR/config 
+echo "    IdentityFile "$SSHDIR/id_rsa >> $SSHDIR/config
+echo "    User git" >> $SSHDIR/config
 
 xclip < $SSHDIR/id_rsa.pub
 echo "The SSH public key is now copied! Paste it on github's \"Add an SSH Key\" page."
