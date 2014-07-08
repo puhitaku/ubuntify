ec18cca1d105dfd91e03c9d7c725cae576647373
puhitaku <puhitaku@gmail.com>
2014-07-08 10:40:53 -0700

    removed unused line

--- base.sh
+++ base.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-SSHDIR="~/.ssh"
-
 echo "PPA repos are going to be installed. Proceed with installation pressing Enter."
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
 sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
