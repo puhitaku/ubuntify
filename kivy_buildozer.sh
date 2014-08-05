aba6b9aebaf38d7c4dc640878470c86b4fe306ca
puhitaku <puhitaku@gmail.com>
2014-08-05 08:45:30 -0700

    Java functionality

--- kivy_buildozer.sh
+++ kivy_buildozer.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
-echo "Please make sure that you have installed JDK + Android SDK."
+echo "Please make sure that you have installed Android SDK."
+echo "JDK will be installed automatically."
 
 pip install buildozer
 sudo apt-get install lib32stdc++6 lib32z1
+
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update
+sudo apt-get install oracle-java7-installer
